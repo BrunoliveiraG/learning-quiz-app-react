d8374bf46d9c270aff6e9a74a4baa2202da022b3
brunoliveiraUNB <brunoliveiragomesz@gmail.com>
2023-02-15 23:07:02 -0300

    remove some questions

--- src/data/questions_complete.js
+++ src/data/questions_complete.js
@@ -31,63 +31,6 @@ const data = [
       },
     ],
   },
-  {
-    category: "Matemática",
-    questions: [
-      {
-        question: "Qual regra altera a cor de um elemento?",
-        options: ["color", "background-color", "font-size", "transition"],
-        answer: "color",
-        tip: "Cor em inglês",
-      },
-      {
-        question: "Para aumentar a fonte de um elemento utilizamos:",
-        options: ["font", "text-transform", "font-size", "hover"],
-        answer: "font-size",
-      },
-      {
-        question: "A posição que deixa um elemento fixo é a:",
-        options: ["static", "absolute", "fixed", "relative"],
-        answer: "fixed",
-      },
-    ],
-  },
-  {
-    category: "Biologia",
-    questions: [
-      {
-        question: "O que é Vanilla JavaScript?",
-        options: [
-          "JavaScript puro",
-          "Uma biblioteca JavaScript",
-          "Um framework JavaScript",
-          "Um compilador de JavaScript",
-        ],
-        answer: "JavaScript puro",
-      },
-      {
-        question: "Com qual instrução declaramos uma constante em JavaScript?",
-        options: ["const", "let", "var", "define"],
-        answer: "const",
-      },
-      {
-        question: "Qual dos tipos de dado a seguir não existe em JavaScript?",
-        options: ["string", "number", "boolean", "float"],
-        answer: "float",
-      },
-      {
-        question: "Qual dos métodos a seguir seleciona um elemento?",
-        options: ["querySelector", "parseInt", "sort", "reduce"],
-        answer: "querySelector",
-      },
-      {
-        question:
-          "Qual destas propriedades da a quantidade de elementos de um array?",
-        options: ["qty", "length", "items", "index"],
-        answer: "length",
-      },
-    ],
-  },
 ];
 
 export default data;
